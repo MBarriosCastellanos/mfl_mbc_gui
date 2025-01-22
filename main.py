709670fdd01038883c69135fb3ef977541665b3f
Mauricio Barrios Castellanos <mcastellanos@cic.uis.edu.co>
2025-01-22 17:43:22 -0500

    elapsed time consideration

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ class DataAdquisition:
     self.msm_size = struct.calcsize(        # Tamaño del mensaje
         self.bin_msm_format) + len(";****".encode())
     self.ports = list_ports.comports()  # Puertos seriales disponibles
-    self.ports = [                      # ìstar puertos seriales            
+    self.ports = [                      # Listar puertos seriales            
       port.device for port in self.ports]
 
     self.identify_comm_mfl()
@@ -59,23 +59,25 @@ class DataAdquisition:
         print(f"Puerto {port} abierto exitosamente.")
       except Exception as e:          # Manejar excepción
         print(f"No se pudo abrir el puerto {port}: {e}")  
+    self.buffers = [bytearray() for _ in self.ports]
 
   def close_serial_ports(self):
     for comm in self.serial_connections:
       comm.close()
       print(f"Puerto {comm.port} cerrado exitosamente.")
 
-  def read_port_data(self, comm, buffer):
+  def read_port_data(self, i):
+    comm = self.serial_connections[i]
     data = comm.read(comm.in_waiting or self.msm_size) # Leer datos
-    buffer.extend(data)                     # Agregar datos al buffer
-    end = buffer.find(b";****")             # Buscar fin de línea
+    self.buffers[i].extend(data)                     # Agregar datos al buffer
+    end = self.buffers[i].find(b";****")             # Buscar fin de línea
     values, body = None, None
     if end >= 0:                            # Si se encontró el fin de línea
-      message = buffer[:end]                # Extraer mensaje
-      buffer = buffer[end + len(b";****"):] # Actualizar buffer
+      message = self.buffers[i][:end]                # Extraer mensaje
+      self.buffers[i] = self.buffers[i][end + len(b";****"):] # Actualizar buffer
       if len(message) == self.msm_size - len(b";****"): # Verificar tamaño del mensaje
-        values, body = self.decode_serial_message(message) # Decodificar mensaje
-    return buffer, values, body
+        return self.decode_serial_message(message) # Decodificar mensaje
+    return values, body
 
   def identify_comm_mfl(self):
     # Paso 1: Identificar qué puerto corresponde a cada cuerpo
@@ -86,13 +88,16 @@ class DataAdquisition:
 
     # Mientras no se hayan identificado los 3 cuerpos
     while len(port_to_body) < 3:
-      for port, comm in zip(self.ports, self.serial_connections):
+      for i in range(len(self.ports)):
+        port = self.ports[i]
+        comm = self.serial_connections[i]
         if port in port_to_body.values():
           continue
+        if len(port_to_body)>=3:
+          break
         attempts = 0
-        buffer = bytearray()
         while attempts < max_attempts:
-          buffer, _ , body = self.read_port_data(comm, buffer)
+          _ , body = self.read_port_data(i)
           if body is not None:
             port_to_body[body] = port
             print(f"El puerto {port} corresponde al cuerpo {body + 1}")
@@ -100,34 +105,43 @@ class DataAdquisition:
             break
           attempts += 1 
           if attempts == max_attempts and port not in port_to_body.values():
-            print(f"No se pudo identificar un cuerpo para el puerto {port} después de {max_attempts} intentos.")
+            print(f"No se pudo identificar un cuerpo para el puerto\
+               {port} después de {max_attempts} intentos.")
     self.ports = [port_to_body[i] for i in sorted(port_to_body.keys())]
     print(f"Los puertos identificados en orden son {self.ports}")
 
   def publish_data(self):
     self.open_serial_ports()
     n_ports = len(self.ports)
-    buffers = [bytearray() for _ in range(n_ports)]
     buffer_acquisition = {i: [] for i in range(n_ports)}
     iterations = 0
+    target_frequency = 350  # Frecuencia en Hz
+    target_period = 1 / target_frequency  # Período objetivo en segundos
+
     try:
       while True:
+        start_time_bucle = time.time()
+        #results = map(self.read_port_data, range(n_ports))
+        #for values, body in results:
         for i in range(n_ports):
-          buffers[i], values, body = self.read_port_data(
-            self.serial_connections[i], 
-            buffers[i]
-            )
-          if body is not None and iterations>600:
+          values, body = self.read_port_data(i)
+          if body is not None:
             buffer_acquisition[body].append(values)
         iterations +=1
         if all(len(lst) >= 300 for lst in buffer_acquisition.values()):
-          print("elapsed time : %.4f =================" % (time.time() - start_time))
+          print("elapsed time : %.4f, Iterations %s =================" % (
+            (time.time() - start_time), iterations))
           for i, data in buffer_acquisition.items():
             size = len(data)  # Obtiene directamente la longitud de la lista
             print(f"Se leyeron {size} datos del cuerpo {i}")
           buffer_acquisition = {i: [] for i in range(n_ports)}
-
-          
+          iterations = 0
+        # Control del tiempo para mantener la frecuencia de 300 Hz
+        elapsed_time_bucle = time.time() - start_time_bucle  # Tiempo transcurrido en esta iteración
+        remaining_time = target_period - elapsed_time_bucle
+        if remaining_time > 0:
+            time.sleep(remaining_time)  # Espera el tiempo necesario para mantener 300 Hz
+        
     except KeyboardInterrupt:
       self.close_serial_ports()
       print(f"Se leyeron {iterations} iteraciones de datos.")
@@ -135,7 +149,6 @@ class DataAdquisition:
 
 if __name__ == "__main__":
   adquisition = DataAdquisition()
-  adquisition.publish_data()
 
 
 
